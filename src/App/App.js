b00fdf2a91d31558fc0fabfea028326bcb534a09
Danielle Cardona <danielle.cardona23@gmail.com>
2024-12-12 16:08:37 -0700

    feat: movie poster styling

--- src/App/App.js
+++ src/App/App.js
@@ -15,8 +15,14 @@ function App() {
       </header>
       <section>
         {moviePosters.map((poster) =>
-        <section key={poster.id}>
-          <img src={poster.poster_path} alt={poster.title}></img>
+        <section key={poster.id} className='movie-poster'>
+          <div className='poster-container'>
+            <img className='poster' src={poster.poster_path} alt={`Poster of ${poster.title}`}></img>
+            <div className='vote-banner'>
+              <p>{poster.vote_count}</p>
+            </div>
+          </div>
+          
         </section>
         )}
       </section> 
