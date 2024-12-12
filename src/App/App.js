0da4f8d57fc14dcb9fb838147421270405f76a88
Danielle Cardona <danielle.cardona23@gmail.com>
2024-12-12 15:28:58 -0700

    feat: display movie posters

--- src/App/App.js
+++ src/App/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import searchIcon from '../icons/search.png';
 
 // Example imports (for later):
-// import { useState, useEffect } from 'react';
-// import moviePosters from '../data/movie_posters';
+import { useState, useEffect } from 'react';
+import moviePosters from '../data/movie_posters';
 // import movieDetails from '../data/movie_details';
 // import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
@@ -13,6 +13,13 @@ function App() {
       <header>
         <h1>rancid tomatillos</h1>
       </header>
+      <section>
+        {moviePosters.map((poster) =>
+        <section key={poster.id}>
+          <img src={poster.poster_path} alt={poster.title}></img>
+        </section>
+        )}
+      </section> 
     </main>
   );
 }
