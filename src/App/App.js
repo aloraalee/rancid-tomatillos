350fa5f3ecbdb7999a7598e6acdf80da9afa8a5e
Danielle Cardona <danielle.cardona23@gmail.com>
2024-12-13 11:06:12 -0700

    refactor to pass posters as props to movie posters

--- src/App/App.js
+++ src/App/App.js
@@ -2,40 +2,24 @@ import './App.css';
 // import searchIcon from '../icons/search.png';
 
 // Example imports (for later):
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import moviePosters from '../data/movie_posters';
-import upvote from '../icons/upvote.png';
-import downvote from '../icons/downvote.png'
+
 // import movieDetails from '../data/movie_details';
-// import MoviesContainer from '../MoviesContainer/MoviesContainer';
+import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
 function App() {
+  const [posters] = useState(moviePosters)
+
   return (
     <main className='App'>
       <header>
         <h1>rancid tomatillos</h1>
       </header>
-      <section>
-        {moviePosters.map((poster) =>
-        <section key={poster.id} className='movie-poster'>
-          <div className='poster-container'>
-            <img className='poster' src={poster.poster_path} alt={`Poster of ${poster.title}`}></img>
-            <div className='vote-banner'>
-              <button className='vote-button'>
-                <img className='upvote' src={upvote} alt="Up vote button"/>
-              </button>
-              <p>{poster.vote_count}</p>
-              <button className='vote-button'>
-                <img className='downvote' src={downvote} alt="Down vote button"/>
-              </button>
-            </div>
-          </div>
-          
-        </section>
-        )}
-      </section> 
+      <MoviesContainer moviePosters={posters}/>
     </main>
   );
 }
 
 export default App;
+
--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -1,11 +1,19 @@
+import MoviePoster from '../MoviePoster/MoviePoster';
 import './MoviesContainer.css';
 
-function Movies() {
+function MoviesContainer({moviePosters}) {
   return (
-      <section className='MoviesContainer'>
-        <p>We'll make some movie posters show up here!</p>
-      </section>
-  );
-}
-  
-export default Movies;
+    <section className="movies-container">
+      {moviePosters.map(poster => (
+        <MoviePoster
+          key={poster.id}
+          img={poster.poster_path}
+          title={poster.title}
+          voteCount={poster.vote_count}
+      />
+      ))}
+    </section>
+  )
+  }
+
+export default MoviesContainer;
