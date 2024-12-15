e27c400fa008ec358b3736b7be165a566c9fadd1
Alora Riley <alora.riley@gmail.com>
2024-12-15 17:53:09 -0500

    fix: Change genre to genre_ids and set foundation for CSS

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -3,9 +3,11 @@ import './MovieDetails.css';
 function MovieDetails({ selectedMovie}) {
   return (
     <section className='MovieDetails'>
+      <h2>{selectedMovie.title}</h2>
+      <div className='genres'>
+        {selectedMovie.genre_ids.join(' ')}>
+      </div>
       <p>
-        {selectedMovie.title}
-        {selectedMovie.genre}
         {selectedMovie.overview}
       </p>
     </section>
