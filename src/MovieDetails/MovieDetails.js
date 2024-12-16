6db2d95f4e20835df207a0d9d1df88247c3c3382
Alora Riley <alora.riley@gmail.com>
2024-12-15 21:36:44 -0500

    feat: Change genre so that each element is separated into individual elements for styling.

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -2,14 +2,15 @@ import './MovieDetails.css';
 
 function MovieDetails({ selectedMovie}) {
   return (
-    <section className='MovieDetails'>
+    <section className='movie-details'>
+      <img src={selectedMovie.backdrop_path} alt={`Movie details for ${selectedMovie.title}`}/>
       <h2>{selectedMovie.title}</h2>
-      <div className='genres'>
-        {selectedMovie.genre_ids.join(' ')}>
+      <div className='genre'>
+        {selectedMovie.genre_ids.map((genre, index) => (
+          <span key={index}>{genre}</span>
+        ))}
       </div>
-      <p>
-        {selectedMovie.overview}
-      </p>
+      <p>{selectedMovie.overview}</p>
     </section>
   );
 }
