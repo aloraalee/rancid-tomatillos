f975fee3fb7d36ca2faf73350fb327b4c07a03fe
Alora Riley <alora.riley@gmail.com>
2024-12-18 18:09:44 -0500

    feat: Create functionality for the home button to not show on the home page.

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,26 @@
 import './App.css';
-<<<<<<< HEAD
-// import searchIcon from '../icons/search.png';
-import home from '../icons/home.png';
-// Example imports (for later):
 import { useState, useEffect } from 'react';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 import MovieDetails from '../MovieDetails/MovieDetails';
+import { Routes, Route, Link } from 'react-router-dom'
+import home from '../icons/home.png';
 import { useLocation } from 'react-router-dom';
 
 function App() {
   const [posters, setPosters] = useState([])
-  const [selectedMovie, setSelectedMovie] = useState(null)
-  const location = useLocation();
-=======
-import { useState, useEffect } from 'react';
-import MoviesContainer from '../MoviesContainer/MoviesContainer';
-import MovieDetails from '../MovieDetails/MovieDetails';
-import { Routes, Route } from 'react-router-dom'
-
-function App() {
-  const [posters, setPosters] = useState([])
   const [searchQuery, setSearchQuery] = useState('')
->>>>>>> 206bff0a4040cfb70fd40b4de63fc428c0c3b3ae
+  const [displayHomeBtn, setHomeBtn] = useState('hide-home-btn')
+  const location = useLocation()
+
+function changeHomeBtn() {
+  setHomeBtn('show-home-btn')
+  }
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setHomeBtn('hide-home-btn');
+    }
+  }, [location]);
 
 function displayPosters() {
   fetch("https://rancid-tomatillos-api.onrender.com/api/v1/movies")
@@ -74,23 +73,23 @@ useEffect(() => {
     <main className='App'>
       <header>
         <h1>rancid tomatillos</h1>
-<<<<<<< HEAD
-        {location.pathname !== '/' && <button className='home-btn' onClick={() => displayPosters()}>
-          <img className='home' src={home} alt="Home button"/>
-       </button>}
-=======
         <input className='search'
           type="text"
           placeholder="Search for a movie"
           value={searchQuery}
           onChange={(event) => setSearchQuery(event.target.value)}
         />
->>>>>>> 206bff0a4040cfb70fd40b4de63fc428c0c3b3ae
+      <Link to={'/'}>
+        <button className={displayHomeBtn}>
+        <img className='home' src={home} alt="Home button"/>
+        </button>
+      </Link>
       </header>
       <Routes>
         <Route path='/' element={<MoviesContainer posters={filteredPosters} 
           incrementVoteDown={incrementVoteDown} 
-          incrementVoteUp={incrementVoteUp}/>}/>
+          incrementVoteUp={incrementVoteUp}
+          changeHomeBtn={changeHomeBtn}/>}/>
         <Route path='/:id' element={<MovieDetails
           displayPosters={displayPosters} />}/>
       </Routes>
